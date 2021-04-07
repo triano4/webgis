05ae6991778fcdb6ce22e0bd81c6b48f0fe6d68a
trianom4 <trianom4@gmail.com>
2021-04-07 15:40:17 +0700

    update js

--- script.js
+++ script.js
@@ -51,36 +51,43 @@ windyInit(options, windyAPI => {
     // 'picker' and other usefull stuff
 
     const { map } = windyAPI;
-    const { overlays, broadcast } = windyAPI;
-
-    const mapMetric = overlays.map;
+    
     // .map is instance of Leaflet map
 
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        //     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1
+        }).addTo(map);
     // L.popup()
     //     .setLatLng([-7.50166466, 109.637832302])
     //     .setContent('Ini Pulau Jawa')
     //     .openOn(map);
     // L.control.scale().addTo(map);
+    // console.log()
+    // var grayscale = L.tileLayer(mapboxUrl, {id: 'MapID', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution}),
+    // streets   = L.tileLayer(mapboxUrl, {id: 'MapID', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution});
+
+    // var map = L.map('map', {
+    //     center: [39.73, -104.99],
+    //     zoom: 10,
+    //     layers: [grayscale, cities]
+    // });
+    // var baseMaps = {
+    //     "Grayscale": grayscale,
+    //     "Streets": streets
+    // };
+    
+    // var overlayMaps = {
+    //     "Cities": cities
+    // };
+    // L.control.layers(baseMaps, overlayMaps).addTo(map);
+    // var baseMaps = {
+    //     "<span style='color: gray'>Grayscale</span>": grayscale,
+    //     "Streets": streets
+    // };
  
 });
-
-// windyInit(options, windyAPI => {
-//     // All the params are stored in windyAPI.store
-//     const { overlays, broadcast } = windyAPI;
-
-//     const windMetric = overlays.wind.metric;
-//     console.log(windMetric);
-//     // 'kt' .. actually selected metric for wind overlay
-//     // Read only value! Do not modify.
-
-//     overlays.wind.listMetrics();
-//     // ['kt', 'bft', 'm/s', 'km/h', 'mph'] .. available metrics
-
-//     overlays.wind.setMetric('m/s');
-//     // Metric for wind was changed to bft
-
-//     broadcast.on('metricChanged', (overlay, newMetric) => {
-//         // Any changes of any metric can be observed here
-//         console.log(overlay, newMetric);
-//     });
-// });
